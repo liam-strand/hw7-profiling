9d0a359ff195182236bd12fb6723845c36070d68
Liam Strand <Liam.Strand@tufts.edu>
2022-04-21 01:06:49 -0400

    start work on Seq replacement

---
+++ um-code/segmented_mem.h
@@ -0,0 +1,22 @@
+#ifndef SEGMENTED_MEM_INCLUDED
+#define SEGMENTED_MEM_INCLUDED
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef struct Segmented_Mem_T *Segmented_Mem_T;
+
+extern Segmented_Mem_T Segments_New(unsigned hint);
+extern void            Segments_Free(Segmented_Mem_T *segs_p);
+
+extern uint32_t *Segments_Access(Segmented_Mem_T mem, uint32_t seg_id);
+extern uint32_t  Segments_Get(Segmented_Mem_T mem, uint32_t seg_id, uint32_t idx);
+extern void      Segments_Put(Segmented_Mem_T mem, uint32_t seg_id, uint32_t idx, uint32_t value);
+extern uint32_t  Segments_Size(Segmented_Mem_T mem, uint32_t seg_id);
+
+
+extern uint32_t Segments_Map(Segmented_Mem_T mem, uint32_t size, size_t index);
+extern void     Segments_Unmap(Segmented_Mem_T mem, uint32_t seg_id);
+
+#endif /* SEGMENTED_MEM_INCLUDED */
